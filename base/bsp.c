96cb3406c4a9b47228665fc0eec960913120bc92
Allen Aby <34366368+AllenAby@users.noreply.github.com>
2020-01-16 17:22:30 -0800

    Declaring and Implementing bspI2CInit function

--- base/bsp.c
+++ base/bsp.c
@@ -1,1 +1,10 @@
 #include "bsp.h"
+
+void bspI2CInit() {
+   // Stop watchdog timer
+   WDTCTL = WDTPW | WDTHOLD;
+   
+   // Disable the GPIO power-on default high-impedance mode to activate
+   // previously configured port settings
+   PM5CTL0 &= ~LOCKLPM5;
+}
