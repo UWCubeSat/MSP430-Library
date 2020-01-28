ce2e8bb9d33d0ff64897c13d4c134a993d72a736
Logan Ince <logan_ince@outlook.com>
2020-01-27 18:22:04 -0800

    Still in the process of finding necessary declarations and pulling them in to define read and write, so far it has felt like a lot of copy paste, will probably need help pruning during team work meeting

--- I2C/I2C.c
+++ I2C/I2C.c
@@ -7,9 +7,50 @@
 
 #include "I2C.h";
 
+// Initializes I2C
+// UCB0CTL (eUSCI_B control word)
+// UCMODE (Clock controls)
+// UCB0BR0 (eUSCI_B bit rate)
 
+hdev i2c_init(bus_instance_i2c bus, uint8_t slave_addr) {
 
-typedef uint8_t hDev;
+    // Ensure USCI_B0 is reset before configuration
+    UCB0CTL1 |= UCSWRST;
+
+    // Set USCI_B0 to master mode I2C mode
+    UCB0CTLW0 |= UCMST + UCMODE_3;
+
+    // Configure the baud rate registers for 100kHz when sourcing from SMCLK
+    // where SMCLK = 1MHz
+    UCB0BR0 = 10;
+    UCB0BR1 = 0;
+
+    // automatic STOP assertion
+    UCB0CTLW1 = UCASTP_2;
+
+    // TX 7 bytes of data
+    UCB0TBCNT = 0x07;
+
+    // Address of slave is 12 hex
+    UCB0I2CSA = slave_addr;
+
+    // eUSCI_B in operational state
+    UCB0CTL1 &= ^UCSWRST;
+
+    // Enable TX-interrupt
+    UCB0IE |= UCTXIE;
+
+    // General interrupt enable
+    GIE;
+
+    // Fill TX buffer
+    UCB0TXBUF = 0x77;
+
+    // Take USCI_B0 out of reset and source clock from SMCLK
+    UCB0CTL1 = UCSSEL_2;
+
+    return 0;
+}
 
 static i2c_result i2cCoreRead(hDev device, uint8_t * buff, uint8_t szToRead, BOOL initialAutoStopSetup)
 {
@@ -109,99 +150,4 @@ static i2c_result i2cCoreWrite(hDev device, uint8_t * buff, uint8_t szToWrite, B
     bytesWritten += szToWrite;
     lastOperation = i2cRes_noerror;
     return i2cRes_noerror;
-}
-
-i2c_result i2cMasterRead(hDev device, uint8_t * buff, uint8_t szToRead)
-{
-    return i2cCoreRead(device, buff, szToRead, TRUE);
-}
-i2c_result i2cMasterWrite(hDev device, uint8_t * buff, uint8_t szToWrite)
-{
-    return i2cCoreWrite(device, buff, szToWrite, TRUE);
-}
-
-i2c_result i2cMasterCombinedWriteRead(hDev device, uint8_t * wbuff, uint8_t szToWrite, uint8_t * rbuff, uint8_t szToRead)
-{
-    bus_instance_i2c bus = devices[device].bus;
-
-    i2cWaitForStopComplete(bus);
-
-    i2cDisable(bus);
-    i2cAutoStopSetTotalBytes(bus, szToWrite + szToRead);
-    i2cEnable(bus);
-    lastOperation = i2cCoreWrite(device, wbuff, szToWrite, FALSE);
-    if(lastOperation)
-        return lastOperation;
-
-    i2cWaitReadyToTransmitByte(bus);
-    if((I2CREG(bus, UCBxIFG) & UCTXIFG0) == 0)
-    {
-        lastOperation = i2cRes_transmitTimeout;
-        return i2cRes_transmitTimeout;
-    }
-    // CoreRead sets the last operation
-    return i2cCoreRead(device, rbuff, szToRead, FALSE);
-}
-
-i2c_result i2cMasterRegisterRead(hDev device, uint8_t registeraddr, uint8_t * buff, uint8_t szToRead)
-{
-    bus_instance_i2c bus = devices[device].bus;
-
-    i2cWaitForStopComplete(bus);
-    if(I2CREG(bus, UCBxCTLW0) & UCTXSTP)
-    {
-        lastOperation = i2cRes_stopTimeout;
-        return i2cRes_stopTimeout;
-    }
-
-    return i2cMasterCombinedWriteRead(device, &registeraddr, 1, buff, szToRead);
-}
-
-void i2cReset()
-{
-    // the I2C bus numbering is 1-indexed
-    uint8_t i;
-    for (i = 1; i < CONFIGM_i2c_maxperipheralinstances; i++)
-    {
-        if (buses[i].initialized)
-        {
-            bus_instance_i2c bus = (bus_instance_i2c)i;
-            i2cDisable(bus);
-            bspI2CInit(bus);
-            i2cEnable(bus);
-        }
-    }
-    busErrorCount = 0;
-}
-
-hDev i2cInit(bus_instance_i2c bus, uint8_t slaveaddr)
-{
-    bus_context_i2c * pBus = &(buses[(uint8_t)bus]);
-
-    // First handle bus initialization, if necessary
-    if (!pBus->initialized)
-    {
-        pBus->initialized = TRUE;
-
-        bspI2CInit(bus);
-        populateBusRegisters(bus);
-
-        // USCI Configuration:  Common for both TX and RX
-        i2cDisable(bus);                                             // Software reset enabled
-        I2CREG(bus, UCBxCTLW0) |= UCMODE_3 | UCMST | UCSYNC | UCSSEL__SMCLK;     // I2C, master, synchronous, use SMCLK
-        I2CREG(bus, UCBxBRW) = 12;                                               // Baudrate = SMCLK / 12
-        I2CREG(bus, UCBxCTLW1) |= UCASTP_2;                                      // Automatic stop generated after transmission complete
-        I2CREG(bus, UCBxCTLW1) |= UCCLTO_3;                                      // clk low timeout @ 34ms
-        i2cEnable(bus);                                             // Software reset disabled
-    }
-
-    // Now setup the actual individual device
-    uint8_t currindex = numDevices;
-    numDevices++;
-    pBus->num_devices++;
-
-    devices[currindex].bus = bus;
-    devices[currindex].slaveaddr = slaveaddr;
-
-    return (hDev)currindex;
 }
--- I2C/I2C.h
+++ I2C/I2C.h
@@ -10,8 +10,46 @@
 
 #include <msp430.h>
 #include <stdint.h>
+#include <bsp.c>
 
+// Copy pasted from i2c.h
+typedef enum _bus_instance_i2c {
+    I2CBus1 = 1,
+    I2CBus2 = 2,
+} bus_instance_i2c;
 
+typedef enum {
+    i2cRes_noerror = 0,
+    i2cRes_startTimeout = 1,
+    i2cRes_stopTimeout = 2,
+    i2cRes_nack = 3,
+    i2cRes_transmitTimeout = 4
+} i2c_result;
+
+typedef struct _bus_registers_i2c {
+    volatile uint16_t * UCBxCTLW0;
+    volatile uint8_t * UCBxCTL1;
+    volatile uint16_t * UCBxCTLW1;
+    volatile uint16_t * UCBxBRW;
+    volatile uint16_t * UCBxSTATW;
+    volatile uint16_t * UCBxTBCNT;
+    volatile uint16_t * UCBxRXBUF;
+    volatile uint16_t * UCBxTXBUF;
+    volatile uint16_t * UCBxI2CSA;
+    volatile uint16_t * UCBxIE;
+    volatile uint16_t * UCBxIFG;
+    volatile uint16_t * UCBxIV;
+} bus_registers_i2c;
+
+// Copy pasted from i2c.h Low Level functions, need to find declaration of bus_instance_i2c
+FILE_STATIC void inline i2cDisable(bus_instance_i2c bus)  { I2CREG(bus, UCBxCTLW0) |= UCSWRST; }
+void inline i2cEnable(bus_instance_i2c bus)  { I2CREG(bus, UCBxCTL1) &= ~UCSWRST; }
+FILE_STATIC void inline i2cMasterTransmitStart(bus_instance_i2c bus)  { I2CREG(bus, UCBxCTL1) |= UCTR | UCTXSTT; }
+FILE_STATIC void inline i2cMasterTransmitStop(bus_instance_i2c bus) { I2CREG(bus, UCBxCTL1) |= UCTXSTP; }
+FILE_STATIC void inline i2cMasterReceiveStart(bus_instance_i2c bus)  { I2CREG(bus, UCBxCTL1) &= ~UCTR;  I2CREG(bus, UCBxCTL1) |= UCTXSTT;  }
+FILE_STATIC void inline i2cLoadTransmitBuffer(bus_instance_i2c bus, uint8_t input)  {  I2CREG(bus, UCBxTXBUF) = input; }
+FILE_STATIC uint8_t inline i2cRetrieveReceiveBuffer(bus_instance_i2c bus)  {  return I2CREG(bus, UCBxRXBUF);  }
+FILE_STATIC void inline i2cAutoStopSetTotalBytes(bus_instance_i2c bus, uint8_t count)  { I2CREG(bus, UCBxTBCNT) = count; }  // NOTE: must be called under reset!
 
 // CONFIGM... configuration values are defined as MACROS
 // Use these for values that will be needed in other initializers (best example:  buffer sizes)
@@ -36,7 +74,7 @@ i2c_result i2cMasterCombinedWriteRead(hDev device, uint8_t * wbuff, uint8_t szTo
 // Reset all Previously Initialized I2C Buses.
 void i2cReset();
 
-//
+// Initializes I2C
 hDev i2cInit(bus_instance_i2c instance, uint8_t slaveaddr);
 
 
