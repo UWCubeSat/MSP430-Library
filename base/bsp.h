d3848701b214f7e6f1ff2e54b81789c27a2da637
Allen Aby <34366368+AllenAby@users.noreply.github.com>
2020-01-30 16:24:24 -0800

    Defined variables for hexadecimal values in header file

--- base/bsp.h
+++ base/bsp.h
@@ -1,3 +1,9 @@
 #include <msp430.h>
 
+extern int BAUDRATE = 0x008; /* declared in bsp.c */
+extern int TX = 0x07; /* declared in bsp.c */
+extern int SLVADD = 0x0012; /* declared in bsp.c */
+extern int I2CPN = 0x03; /* declared in bsp.c */
+extern int TXBFFR = 0x77; /* declared in bsp.c */
+
 void bspI2CInit();
