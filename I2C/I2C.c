57738ecb6807e56f76a61563d50fcbc9533d0f03
Logan Ince <logan_ince@outlook.com>
2020-01-31 11:53:41 -0800

    Added all functions and declarations from old version, added additional comments to provide clarity and note questions for the intended direction of the I2C library going forward. Still won't accept uintX_t vars and constants

--- I2C/I2C.c
+++ I2C/I2C.c
@@ -5,13 +5,59 @@
  *      Author: Logan Ince
  */
 
-#include "I2C.h";
+/*
+ * Lots of the below is copy pasted from jeffc's i2c.c
+ * Changing what should be changed to facilitate better legibility for future,
+ * primarily through commenting, though will refactor if necessary.
+ */
+
+#include <I2C.h>
+
+uint16_t busErrorCount = 0;
+uint16_t i2cGetBusErrorCount()
+{
+    return busErrorCount;
+}
+
+uint16_t bytesRead = 0;
+uint16_t i2cGetBytesRead()
+{
+    return bytesRead;
+}
+
+uint16_t bytesWritten = 0;
+uint16_t i2cGetBytesWritten()
+{
+    return bytesWritten;
+}
+
+i2c_result lastOperation = i2cRes_noerror;
+i2c_result i2cGetLastOperationResult()
+{
+    return lastOperation;
+}
+
+void i2cReset()
+{
+    // the I2C bus numbering is 1-indexed
+    uint8_t i;
+    for (i = 1; i < CONFIGM_i2c_maxperipheralinstances; i++)
+    {
+        if (buses[i].initialized)
+        {
+            bus_instance_i2c bus = (bus_instance_i2c)i;
+            i2cDisable(bus);
+            bspI2CInit(bus);
+            i2cEnable(bus);
+        }
+    }
+    busErrorCount = 0;
+}
 
 // Initializes I2C
 // UCB0CTL (eUSCI_B control word)
 // UCMODE (Clock controls)
 // UCB0BR0 (eUSCI_B bit rate)
-
 hdev i2c_init(bus_instance_i2c bus, uint8_t slave_addr) {
 
     // Ensure USCI_B0 is reset before configuration
@@ -51,6 +97,46 @@ hdev i2c_init(bus_instance_i2c bus, uint8_t slave_addr) {
 
     return 0;
 }
+
+// Some quasi-unpleasant hackery to make for clean dual-bus support in general
+static void populateBusRegisters(bus_instance_i2c instance)
+{
+    bus_registers_i2c * pRegs;
+
+    if (instance == I2CBus1)
+    {
+        pRegs = &busregs[1];
+        pRegs->UCBxCTLW0 =  &UCB1CTLW0;
+        pRegs->UCBxCTL1 =   &UCB1CTL1;   // MSB of CTLW0
+        pRegs->UCBxCTLW1 =  &UCB1CTLW1;
+        pRegs->UCBxBRW =    &UCB1BRW;
+        pRegs->UCBxSTATW =  &UCB1STATW;
+        pRegs->UCBxTBCNT =  &UCB1TBCNT;
+        pRegs->UCBxRXBUF =  &UCB1RXBUF;
+        pRegs->UCBxTXBUF =  &UCB1TXBUF;
+        pRegs->UCBxI2CSA =  &UCB1I2CSA;
+        pRegs->UCBxIE =     &UCB1IE;
+        pRegs->UCBxIFG =    &UCB1IFG;
+        pRegs->UCBxIV =     &UCB1IV;
+    }
+    else if (instance == I2CBus2)
+    {
+        pRegs = &busregs[2];
+        pRegs->UCBxCTLW0 =  &UCB2CTLW0;
+        pRegs->UCBxCTL1 =   &UCB2CTL1;   // MSB of CTLW0
+        pRegs->UCBxCTLW1 =  &UCB2CTLW1;
+        pRegs->UCBxBRW =    &UCB2BRW;
+        pRegs->UCBxSTATW =  &UCB2STATW;
+        pRegs->UCBxTBCNT =  &UCB2TBCNT;
+        pRegs->UCBxRXBUF =  &UCB2RXBUF;
+        pRegs->UCBxTXBUF =  &UCB2TXBUF;
+        pRegs->UCBxI2CSA =  &UCB2I2CSA;
+        pRegs->UCBxIE =     &UCB2IE;
+        pRegs->UCBxIFG =    &UCB2IFG;
+        pRegs->UCBxIV =     &UCB2IV;
+    }
+}
+
 
 static i2c_result i2cCoreRead(hDev device, uint8_t * buff, uint8_t szToRead, BOOL initialAutoStopSetup)
 {
@@ -150,4 +236,107 @@ static i2c_result i2cCoreWrite(hDev device, uint8_t * buff, uint8_t szToWrite, B
     bytesWritten += szToWrite;
     lastOperation = i2cRes_noerror;
     return i2cRes_noerror;
+}
+
+2c_result i2cMasterRead(hDev device, uint8_t * buff, uint8_t szToRead)
+{
+    return i2cCoreRead(device, buff, szToRead, TRUE);
+}
+
+i2c_result i2cMasterWrite(hDev device, uint8_t * buff, uint8_t szToWrite)
+{
+    return i2cCoreWrite(device, buff, szToWrite, TRUE);
+}
+
+i2c_result i2cMasterCombinedWriteRead(hDev device, uint8_t * wbuff, uint8_t szToWrite, uint8_t * rbuff, uint8_t szToRead)
+{
+    bus_instance_i2c bus = devices[device].bus;
+
+    i2cWaitForStopComplete(bus);
+
+    i2cDisable(bus);
+    i2cAutoStopSetTotalBytes(bus, szToWrite + szToRead);
+    i2cEnable(bus);
+    lastOperation = i2cCoreWrite(device, wbuff, szToWrite, FALSE);
+    if(lastOperation)
+        return lastOperation;
+
+    i2cWaitReadyToTransmitByte(bus);
+    if((I2CREG(bus, UCBxIFG) & UCTXIFG0) == 0)
+    {
+        lastOperation = i2cRes_transmitTimeout;
+        return i2cRes_transmitTimeout;
+    }
+    // CoreRead sets the last operation
+    return i2cCoreRead(device, rbuff, szToRead, FALSE);
+}
+
+i2c_result i2cMasterRegisterRead(hDev device, uint8_t registeraddr, uint8_t * buff, uint8_t szToRead)
+{
+    bus_instance_i2c bus = devices[device].bus;
+
+    i2cWaitForStopComplete(bus);
+    if(I2CREG(bus, UCBxCTLW0) & UCTXSTP)
+    {
+        lastOperation = i2cRes_stopTimeout;
+        return i2cRes_stopTimeout;
+    }
+
+    return i2cMasterCombinedWriteRead(device, &registeraddr, 1, buff, szToRead);
+}
+
+// Primary interrupt vector for I2C on module B1 (I2C #1)
+#pragma vector = EUSCI_B1_VECTOR
+__interrupt void USCI_B1_ISR(void)
+{
+    switch(__even_in_range(UCB1IV, USCI_I2C_UCBIT9IFG))
+    {
+        case USCI_NONE:          break;     // Vector 0: No interrupts
+        case USCI_I2C_UCALIFG:   break;     // Vector 2: ALIFG
+        case USCI_I2C_UCNACKIFG:            // Vector 4: NACKIFG
+            UCB1CTL1 |= UCTXSTT;            // Received NACK ... try again, start
+            break;
+        case USCI_I2C_UCSTTIFG:  break;     // Vector 6: STTIFG
+        case USCI_I2C_UCSTPIFG:  break;     // Vector 8: STPIFG
+        case USCI_I2C_UCRXIFG3:  break;     // Vector 10: RXIFG3
+        case USCI_I2C_UCTXIFG3:  break;     // Vector 12: TXIFG3
+        case USCI_I2C_UCRXIFG2:  break;     // Vector 14: RXIFG2
+        case USCI_I2C_UCTXIFG2:  break;     // Vector 16: TXIFG2
+        case USCI_I2C_UCRXIFG1:  break;     // Vector 18: RXIFG1
+        case USCI_I2C_UCTXIFG1:  break;     // Vector 20: TXIFG1
+        case USCI_I2C_UCRXIFG0:  break;     // Vector 22: RXIFG0
+        case USCI_I2C_UCTXIFG0:  break;     // Vector 24: TXIFG0
+        case USCI_I2C_UCBCNTIFG: break;     // Vector 26: BCNTIFG
+        case USCI_I2C_UCCLTOIFG: break;     // Vector 28: clock low timeout
+        case USCI_I2C_UCBIT9IFG: break;     // Vector 30: 9th bit
+        default: break;
+    }
+}
+
+// Primary interrupt vector for I2C on module B2 (I2C #2)
+#pragma vector = EUSCI_B2_VECTOR
+__interrupt void USCI_B2_ISR(void)
+{
+    switch(__even_in_range(UCB2IV, USCI_I2C_UCBIT9IFG))
+    {
+        case USCI_NONE:          break;     // Vector 0: No interrupts
+        case USCI_I2C_UCALIFG:   break;     // Vector 2: ALIFG
+        case USCI_I2C_UCNACKIFG:            // Vector 4: NACKIFG
+            UCB2CTL1 |= UCTXSTT;            // Received NACK ... try again, start
+            break;
+        case USCI_I2C_UCSTTIFG:  break;     // Vector 6: STTIFG
+        case USCI_I2C_UCSTPIFG:  break;     // Vector 8: STPIFG
+        case USCI_I2C_UCRXIFG3:  break;     // Vector 10: RXIFG3
+        case USCI_I2C_UCTXIFG3:  break;     // Vector 12: TXIFG3
+        case USCI_I2C_UCRXIFG2:  break;     // Vector 14: RXIFG2
+        case USCI_I2C_UCTXIFG2:  break;     // Vector 16: TXIFG2
+        case USCI_I2C_UCRXIFG1:  break;     // Vector 18: RXIFG1
+        case USCI_I2C_UCTXIFG1:  break;     // Vector 20: TXIFG1
+        case USCI_I2C_UCRXIFG0:  break;     // Vector 22: RXIFG0
+        case USCI_I2C_UCTXIFG0:  break;     // Vector 24: TXIFG0
+        case USCI_I2C_UCBCNTIFG: break;     // Vector 26: BCNTIFG
+        case USCI_I2C_UCCLTOIFG: break;     // Vector 28: clock low timeout
+        case USCI_I2C_UCBIT9IFG: break;     // Vector 30: 9th bit
+        default: break;
+    }
 }
--- I2C/I2C.h
+++ I2C/I2C.h
@@ -5,14 +5,25 @@
  *      Author: Logan Ince
  */
 
-#ifndef I2C_H_
-#define I2C_H_
 
+/*
+ * Lots of the below is copy pasted from jeffc's i2c.h and sat labs other i2c support files
+ * Changing what should be changed to facilitate better legibility for future,
+ * primarily through commenting, though will refactor if necessary.
+ */
+
+/*How do we get uints to become defined if stdint.h does not define it?*/
+
+#include <stdint.h>
 #include <msp430.h>
 #include <stdint.h>
 #include <bsp.c>
 
-// Copy pasted from i2c.h
+
+typedef uint8_t hDev;
+
+/*Should this instead be a different count? What does it affect?*/
+// Keeping it sane with a 1-based count
 typedef enum _bus_instance_i2c {
     I2CBus1 = 1,
     I2CBus2 = 2,
@@ -41,7 +52,38 @@ typedef struct _bus_registers_i2c {
     volatile uint16_t * UCBxIV;
 } bus_registers_i2c;
 
-// Copy pasted from i2c.h Low Level functions, need to find declaration of bus_instance_i2c
+extern bus_registers_i2c busregs[CONFIGM_i2c_maxperipheralinstances];
+
+typedef struct {
+
+} status_i2c;
+
+typedef struct {
+    BOOL initialized;
+    uint8_t num_devices;
+} bus_context_i2c;
+
+typedef struct {
+    bus_instance_i2c bus;
+    uint16_t slaveaddr;
+} device_context_i2c;
+
+//in ms
+#define READ_WRITE_TIMEOUT 50
+#define READ_WRITE_INCREMENT 8
+
+// Core helper definitions
+#define I2C_MASTER_RECEIVE_INTERRUPT_MASK     (UCRXEIE | UCNACKIE )
+#define I2C_MASTER_TRANSMIT_INTERRUPT_MASK    (UCTXIE2 | UCNACKIE )
+
+/*How do we avoid unpleasant hacks to create clean dual-bus support*/
+// Some quasi-unpleasant hacks to make for clean dual-bus support in general
+#define I2CREG(n,x)       (*(busregs[(uint8_t)n].x))
+
+/* Copy pasted from i2c.h Low Level functions, need to find declaration of bus_instance_i2c*/
+/***************************/
+/* I2C LOW-LEVEL FUNCTIONS */
+/***************************/
 FILE_STATIC void inline i2cDisable(bus_instance_i2c bus)  { I2CREG(bus, UCBxCTLW0) |= UCSWRST; }
 void inline i2cEnable(bus_instance_i2c bus)  { I2CREG(bus, UCBxCTL1) &= ~UCSWRST; }
 FILE_STATIC void inline i2cMasterTransmitStart(bus_instance_i2c bus)  { I2CREG(bus, UCBxCTL1) |= UCTR | UCTXSTT; }
@@ -57,6 +99,27 @@ FILE_STATIC void inline i2cAutoStopSetTotalBytes(bus_instance_i2c bus, uint8_t c
 #define CONFIGM_i2c_maxperipheralinstances   3
 #define CONFIGM_i2c_maxdevices               8
 
+// Synchronous (busy-waiting) version of I2C calls
+/*Is this still something we need to do?*/
+// TODO:  Add NACK and other edge-case support to busy/waits
+#ifndef DISABLE_SYNC_I2C_CALLS
+
+/*Is this still something that we need to do?*/
+// TODO:  Re-enable this warning once there is actually an option to do async :)
+//#warning Synchronous calls to I2C are inefficient - consider disabling sync calls and \
+//using the async interrupt-based API instead, with DISABLE_SYNC_I2C_CALLS.
+//FILE_STATIC void inline i2cWaitForStopComplete(bus_instance_i2c bus)  { while (I2CREG(bus, UCBxCTLW0) & UCTXSTP); }
+FILE_STATIC void inline i2cWaitForStopComplete(bus_instance_i2c bus)  { uint16_t timeout = READ_WRITE_TIMEOUT; while (timeout-- && I2CREG(bus, UCBxCTLW0) & UCTXSTP) __delay_cycles(READ_WRITE_INCREMENT); }
+//FILE_STATIC void inline i2cWaitForStartComplete(bus_instance_i2c bus) { while (I2CREG(bus, UCBxCTLW0) & UCTXSTT); }
+FILE_STATIC void inline i2cWaitForStartComplete(bus_instance_i2c bus) { uint16_t timeout = READ_WRITE_TIMEOUT; while (timeout-- && I2CREG(bus, UCBxCTLW0) & UCTXSTT) __delay_cycles(READ_WRITE_INCREMENT); }
+//FILE_STATIC void inline i2cWaitReadyToTransmitByte(bus_instance_i2c bus)  { while ( (I2CREG(bus, UCBxIFG) & UCTXIFG0) == 0); }
+FILE_STATIC void inline i2cWaitReadyToTransmitByte(bus_instance_i2c bus)  { uint16_t timeout = READ_WRITE_TIMEOUT; while (timeout-- && (I2CREG(bus, UCBxIFG) & UCTXIFG0) == 0) __delay_cycles(READ_WRITE_INCREMENT); }
+//FILE_STATIC void inline i2cWaitReadyToReceiveByte(bus_instance_i2c bus)  { while ( (I2CREG(bus, UCBxIFG) & UCRXIFG) == 0); }
+FILE_STATIC void inline i2cWaitReadyToReceiveByte(bus_instance_i2c bus)  { uint16_t timeout = READ_WRITE_TIMEOUT; while (timeout-- && (I2CREG(bus, UCBxIFG) & UCRXIFG) == 0) __delay_cycles(READ_WRITE_INCREMENT); }
+
+/***************************/
+/* I2C MID-LEVEL FUNCTIONS */
+/***************************/
 // Reads data
 i2c_result i2cMasterRead(hDev device, uint8_t * buff, uint8_t szToRead);
 
@@ -69,10 +132,22 @@ i2c_result i2cMasterRegisterRead(hDev device, uint8_t registeraddr, uint8_t * bu
 // Reads and Writes data
 i2c_result i2cMasterCombinedWriteRead(hDev device, uint8_t * wbuff, uint8_t szToWrite, uint8_t * rbuff, uint8_t szToRead);
 
-
+//gets the total count of i2c errors
+uint16_t i2cGetBusErrorCount();
+//gets the result of the last i2c operations
+i2c_result i2cGetLastOperationResult();
+//gets the number of bytes read successfully over i2c
+uint16_t i2cGetBytesRead();
+//gets the number of bytes written successfully over i2c
+uint16_t i2cGetBytesWritten();
 
 // Reset all Previously Initialized I2C Buses.
 void i2cReset();
+
+#endif /* DISABLE_SYNC_I2C_CALLS */
+
+/*Is this something we still need to do?*/
+// TODO:  Add "async" interrupt-based alternative to synchronous versions
 
 // Initializes I2C
 hDev i2cInit(bus_instance_i2c instance, uint8_t slaveaddr);
